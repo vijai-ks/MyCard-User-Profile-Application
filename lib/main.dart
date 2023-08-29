6cd2dbcc604433c7988f182e3fc22edfc4ea9bbc
vijai-ks <vijai.ksv@gmail.com>
2023-08-29 19:10:14 +0530

    feat: profile-page: Add personal details containers
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,81 @@ class MyCard extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      '+91 97866 64633',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'OpenSans',
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      'vijai.ksv@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'OpenSans',
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.location_city,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      'Perundurai, Erode',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'OpenSans',
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
